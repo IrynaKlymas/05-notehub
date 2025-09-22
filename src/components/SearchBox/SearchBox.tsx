268b17aac2aedd88081476fa04b59be88115e7be
Iryna Klymas <klimasir@gmail.com>
2025-09-22 22:44:27 +0300

    feat(GOIT): fix types

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,50 +1,24 @@
-import toast from "react-hot-toast";
-import styles from "./SearchBar.module.css";
+import css from "./SearchBox.module.css";
 
-interface SearchBarProps {
-    onSubmit: (query: string) => void;
+interface SearchBoxProps {
+    value: string;
+    onChange: (newValue: string) => void;
 }
 
-const SearchBar = ({ onSubmit }: SearchBarProps) => {
-    const handleFormAction = (formData: FormData) => {
-        const query = formData.get("query") as string;
-
-        if (query.trim() === "") {
-            toast.error("Please enter your search query.");
-            return;
-        }
-
-        onSubmit(query);
+const SearchBox = ({ value, onChange }: SearchBoxProps) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
     };
 
     return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <a
-                    className={styles.link}
-                    href="https://www.themoviedb.org/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by TMDB
-                </a>
-                {}
-                <form className={styles.form} action={handleFormAction}>
-                    <input
-                        className={styles.input}
-                        type="text"
-                        name="query"
-                        autoComplete="off"
-                        placeholder="Search movies..."
-                        autoFocus
-                    />
-                    <button className={styles.button} type="submit">
-                        Search
-                    </button>
-                </form>
-            </div>
-        </header>
+        <input
+            className={css.input}
+            type="text"
+            placeholder="Search notes"
+            value={value}
+            onChange={handleChange}
+        />
     );
 };
 
-export default SearchBar;
+export default SearchBox;
